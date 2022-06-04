9de8254632ec45039490c8ef39e6ada0342dfd0a
AnhuaiMS <105294731+AnhuaiMS@users.noreply.github.com>
2022-06-04 21:29:08 +0800

    Add files via upload

--- mytestwidget.cpp
+++ mytestwidget.cpp
@@ -2,6 +2,8 @@
 #include "ui_mytestwidget.h"
 #include <QFileDialog>
 #include <QTextStream>
+#include <QBrush>
+#include <QTextCharFormat>
 #include <iostream>
 #include <cstring>
 #include <algorithm>
@@ -9,6 +11,8 @@
 typedef long long LL;
 
 const int maxn=1005;
+
+int f[maxn][maxn];
 
 myTestWidget::myTestWidget(QWidget *parent) :
     QWidget(parent),
@@ -80,6 +84,26 @@ void myTestWidget::on_pushButton_16_clicked()
     ui->textEdit_2->setPlainText("");
 }//右文本框清除
 
+void highlight_out(QTextEdit* te, QString* s){
+    te->setPlainText("");
+    QBrush b1(QColor(0,255,0));
+    QBrush b2(QColor(255,255,255));
+    QTextCharFormat f1, f2;
+    f1.setBackground(b1);
+    f2.setBackground(b2);
+    int len=static_cast<int>(s->length());
+    for(int i=0;i<len;++i){
+        if(s->at(i).toLatin1()=='_') {
+            te->setCurrentCharFormat(f1);
+        }
+        else {
+            te->setCurrentCharFormat(f2);
+        }
+        QString tmp(s->at(i));
+        te->insertPlainText(tmp);
+    }
+}
+
 void my_compare(int x,int y,QString* s1,QString* s2,QString* ans1,QString* ans2,int f[][maxn]){
     if(x==0&&y==0)return;
     if(x==0){
@@ -113,13 +137,14 @@ void myTestWidget::on_pushButton_6_clicked()
     QString s2 = ui->textEdit_2->toPlainText();
     int len1=static_cast<int>(s1.length());
     int len2=static_cast<int>(s2.length());
-    int f[maxn][maxn];
     for(int i=1;i<=len1;i++)
         for(int j=1;j<=len2;j++)
             if(s1.at(i-1)==s2.at(j-1))f[i][j]=f[i-1][j-1]+1;
                 else f[i][j]=std::max(f[i-1][j-1],std::max(f[i-1][j],f[i][j-1]));
     QString ans1="",ans2="";
     my_compare(len1,len2,&s1,&s2,&ans1,&ans2,f);
-    ui->textEdit_1->setText(ans1);
-    ui->textEdit_2->setText(ans2);
+    //ui->textEdit_1->setText(ans1);
+    //ui->textEdit_2->setText(ans2);
+    highlight_out(ui->textEdit_1,&ans1);
+    highlight_out(ui->textEdit_2,&ans2);
 }
