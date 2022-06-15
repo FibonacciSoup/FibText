f95fedfbf0d8e1bc9e2197dd4e15fd390cb00cdd
Sdywolf <33116822+Sdywolf@users.noreply.github.com>
2022-06-15 11:05:14 +0800

    feature update for highlight

--- mytestwidget.cpp
+++ mytestwidget.cpp
@@ -12,7 +12,7 @@ typedef long long LL;
 
 const int maxn=1005;
 
-int f[maxn][maxn];
+static int f[maxn][maxn];
 
 myTestWidget::myTestWidget(QWidget *parent) :
     QWidget(parent),
@@ -84,7 +84,7 @@ void myTestWidget::on_pushButton_16_clicked()
     ui->textEdit_2->setPlainText("");
 }//右文本框清除
 
-void highlight_out(QTextEdit* te, QString* s){
+void highlight_out(QTextEdit* te, QString* s, QString* s1){
     te->setPlainText("");
     QBrush b1(QColor(0,255,0));
     QBrush b2(QColor(255,255,255));
@@ -99,34 +99,34 @@ void highlight_out(QTextEdit* te, QString* s){
         else {
             te->setCurrentCharFormat(f2);
         }
-        QString tmp(s->at(i));
+        QString tmp(s1->at(i));
         te->insertPlainText(tmp);
     }
 }
 
-void my_compare(int x,int y,QString* s1,QString* s2,QString* ans1,QString* ans2,int f[][maxn]){
+void my_compare(int x,int y,QString* s1,QString* s2,QString* ans1,QString* ans2){
     if(x==0&&y==0)return;
     if(x==0){
-        my_compare(x,y-1,s1,s2,ans1,ans2,f);
+        my_compare(x,y-1,s1,s2,ans1,ans2);
         ans2->append('_');
         return;
     }
     if(y==0){
-        my_compare(x-1,y,s1,s2,ans1,ans2,f);
+        my_compare(x-1,y,s1,s2,ans1,ans2);
         ans1->append('_');
         return;
     }
     if(s1->at(x-1)==s2->at(y-1)){
-        my_compare(x-1,y-1,s1,s2,ans1,ans2,f);
+        my_compare(x-1,y-1,s1,s2,ans1,ans2);
         ans1->append(s1->at(x-1));
         ans2->append(s2->at(y-1));
         return;
     }
     if(f[x-1][y]>f[x][y-1]){
-        my_compare(x-1,y,s1,s2,ans1,ans2,f);
+        my_compare(x-1,y,s1,s2,ans1,ans2);
         ans1->append('_');
     }else{
-        my_compare(x,y-1,s1,s2,ans1,ans2,f);
+        my_compare(x,y-1,s1,s2,ans1,ans2);
         ans2->append('_');
     }
 }
@@ -142,9 +142,9 @@ void myTestWidget::on_pushButton_6_clicked()
             if(s1.at(i-1)==s2.at(j-1))f[i][j]=f[i-1][j-1]+1;
                 else f[i][j]=std::max(f[i-1][j-1],std::max(f[i-1][j],f[i][j-1]));
     QString ans1="",ans2="";
-    my_compare(len1,len2,&s1,&s2,&ans1,&ans2,f);
+    my_compare(len1,len2,&s1,&s2,&ans1,&ans2);
     //ui->textEdit_1->setText(ans1);
     //ui->textEdit_2->setText(ans2);
-    highlight_out(ui->textEdit_1,&ans1);
-    highlight_out(ui->textEdit_2,&ans2);
+    highlight_out(ui->textEdit_1,&ans1,&s1);
+    highlight_out(ui->textEdit_2,&ans2,&s2);
 }
